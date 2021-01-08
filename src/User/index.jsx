33254989ed8795fd8eeea783d2c4259d774b4b81
Bruno Ochotorena <hackerhilt@gmail.com>
2021-01-07 22:18:15 -0300

    Mais melhorias

--- src/User/index.jsx
+++ src/User/index.jsx
@@ -2,10 +2,14 @@ import './style.css';
 
 function User({ photo, name, login, location }) {
   return (
-    <li>
+    <li className='userCard'>
       <img className='avatar' src={photo} alt='Foto de usuario'></img>
       {/* <h2>{name}</h2> */}
-      <h2>{login}</h2>/{/* <h2>{location}</h2> */}
+      <h2>{login}</h2>
+      {/* <h2>{location}</h2> */}
+      <button className='userBtn' type='button'>
+        Ver mais...
+      </button>
     </li>
   );
 }
